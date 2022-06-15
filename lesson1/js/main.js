296c2715ebe5f408d2cd3ff0e10ba00511e5e84e
anattok <tokarev182@yandex.ru>
2022-06-15 23:08:34 +0300

    Моё решение задачи из 13-го урока

--- lesson1/js/main.js
+++ lesson1/js/main.js
@@ -1,4 +1,16 @@
  "use strict";
- 
 
- 
+    
+    let numberOfFilm = prompt(` Сколько фильмов вы уже посмотрели`); 
+    let film = prompt(`Назови один из последних просмотренных фильмов`);
+    let raiting = prompt(`На сколько оцените ${film}`);
+
+ let personalMovieDB = {
+    count: numberOfFilm,
+    movies: {[film]: raiting},
+    actors: {},
+    genres: [],
+    privat: false
+ };
+
+ console.log(personalMovieDB);
